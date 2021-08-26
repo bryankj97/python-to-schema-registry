08b31ceb2678de60ae9afb3a79569b9e2e1d48d6
bryankj97 <bryankkj06@gmail.com>
2021-08-26 14:19:35 +0800

    Update schema_registry_client.py

--- schema_registry_client.py
+++ schema_registry_client.py
@@ -1,21 +1,21 @@
 from confluent_kafka import schema_registry
 import json
 
-
-
-client.get_versions('dbserver1.inventory.customers-key')
-schema_instance = client.get_latest_version('dbserver1.inventory.customers-key')
-# Obtain schema id
-schema_instance.schema_id
-
 subject = 'dbserver1.inventory.customers-value'
-
 
 def create_client(url='http://localhost:8081'):
     params = {'url': url}
     client = schema_registry.SchemaRegistryClient(params)
     return client
 
+def get_versions(client, subject='dbserver1.inventory.customers-value'):
+    return client.get_versions(subject)
+
+def get_version(client, subject='dbserver1.inventory.customers-value', version=None):
+    if not version:
+        return client.get_latest_version(subject)
+    else:
+        return client.get_version(subject, version)
 
 def check_existence(client, subject, schema_content):
     available_schemas = client.get_versions(subject)
@@ -32,37 +32,25 @@ def check_existence(client, subject, schema_content):
     return latest_schema_id
 
 
-def update_schema_registry(subject, schema_content):
-    client = 
+def update_schema_registry(client, subject, schema_content):
+    # check if schema already exists
+    old_schema_id = check_existence(client, subject, schema_content)
+    if not old_schema_id:
+        return None
+    old_schema = client.get_schema(old_schema_id)
+    old_schema_str = old_schema.schema_str
+    new_schema_str = json.loads(old_schema_str)
+    # TO-DO:
+    # get updated column from new_schema_str
 
+    # Example:
+    new_schema_str['fields'].append({'name': 'phone', 'type': ['null', 'int'], 'default': None})
+    new_schema_str = json.dumps(new_schema_str)
+    new_schema_str = new_schema_str.replace(" ", "")
+    new_schema = schema_registry.Schema(new_schema_str, 'AVRO')
 
-client.get_schema(6).schema_str
-client.get_schema(15).schema_str
+    # test compataibility
+    client.test_compatibility('dbserver1.inventory.customers-value', new_schema)
 
-schema = json.loads(client.get_schema(15).schema_str)
-schema['fields'][0]['type'][1]['fields'].append({'name': 'number', 'type': ['null', 'int'], 'default': None})
-new_schema_str = str(schema)
-new_schema_str = new_schema_str.replace(" ", "")
-new_schema_str = new_schema_str.replace("'", "")
-
-
-
-schema['fields'][1]['type'][1] = {'type': 'record', 'name': 'Value', 'fields': [{'name': 'address', 'type': 'string'}], 'connect.name': 'dbserver1.inventory.customers.Value'}
-
-schema_str = str(schema)
-
-client.get_schema(6).references
-
-
-new_schema = schema_registry.Schema(new_schema_str, 'AVRO')
-
-new_schema
-
-client.register_schema('dbserver1.inventory.customers-value', new_schema)
-
-
-client.test_compatibility('dbserver1.inventory.customers-value', new_schema, version=1)
-
-
-new_schema.schema_str
-client.get_schema(15).schema_str
+    #register schema
+    client.register_schema('dbserver1.inventory.customers-value', new_schema)
